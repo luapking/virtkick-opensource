77a3ea53cd5c9017159548c42740b56b73bccefd
luapking <162720090+luapking@users.noreply.github.com>
2025-06-04 10:44:32 +0200

    Fix shell scripts and add setup.sh

--- setup/node.sh
+++ setup/node.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-export NVM_DIR=$(pwd)/.nvm
+# Download and initialize a local copy of nvm and Node.js
+NVM_DIR="$(pwd)/.nvm"
+export NVM_DIR
 
 if ! [ -e "$NVM_DIR/installed" ];then
   rm -rf "$NVM_DIR"
@@ -8,8 +10,8 @@ fi
 if ! [ -e "$NVM_DIR" ];then
 #	(git clone https://github.com/creationix/nvm.git .nvm && cd .nvm && git checkout `git describe --abbrev=0 --tags`)
   echo "Downloading Node.JS to local directory"
-  (mkdir -p $NVM_DIR && cd .nvm && wget --quiet https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh)
-	. .nvm/nvm.sh
+  (mkdir -p "$NVM_DIR" && cd .nvm && wget --quiet https://raw.githubusercontent.com/creationix/nvm/master/nvm.sh)
+  . .nvm/nvm.sh
   nvm install 0.10.32
   touch "$NVM_DIR/installed"
 fi
